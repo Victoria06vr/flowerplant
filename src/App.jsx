ea1c4b200a664954c3e485a33306818709f90933
Victoria06vr <eaa25viru@students.eaaa.dk>
2026-03-02 17:23:55 +0100

    sixth

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,22 @@
-import './App.css'
-import { createBrowserRouter, RouterProvider } from "react-router-dom"; 
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from './views/Home'
 import MyPlants from './views/MyPlants'
 import About from './views/About'
 import Layout from './Layout'
-
-// GitHub Pages deploy requires a basename matching the repo name
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      {
-        index: true,
-        element: <Home />
-      },
-      {
-        path: "myplants",
-        element: <MyPlants />
-      },
-      {
-        path: "about",
-        element: <About />
-      }
-    ]
-  }
-],
-{
-  basename: "/flowerplant"
-}
-);
+import './App.css'
 
 function App() {
   return (
-    <RouterProvider router={router} />
+    <BrowserRouter basename="/flowerplant">
+      <Layout />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/myplants" element={<MyPlants />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
-
 
 export default App
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,5 +1,4 @@
 import plantone from "../assets/plantone.jpg"
-import './App.css'
 import { Link } from "react-router-dom";
 
 export default function Home(){
